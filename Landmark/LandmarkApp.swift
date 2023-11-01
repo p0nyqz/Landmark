8abd1121d71bd2766647802789837118103ca1f8
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-11-01 23:59:29 +0100

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by p0nyqz on 1.11.23..
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
