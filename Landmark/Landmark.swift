07e5f060df9579a4f7e27acc6a41120f33527f4d
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-11-04 22:33:32 +0100

    feat: fetch imageName, Coordinates

--- Landmark/Landmark.swift
+++ Landmark/Landmark.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var destriction: String
+    
+    private var imageName: String 
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
