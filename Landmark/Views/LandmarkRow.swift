cb363d350ab108c66ee72ba6742b7227e6d6adda
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-11-05 01:09:07 +0100

    fear: file structure,  ModelData, LandmarkRow fix:Landmark

---
+++ Landmark/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmark
+//
+//  Created by p0nyqz on 5.11.23..
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
