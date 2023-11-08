4360cd1178806e451a505e6c9b334a6efa0c5b94
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-11-08 23:44:32 +0100

    fix: quickfix

--- Landmark/Model/ModelData.swift
+++ Landmark/Model/ModelData.swift
@@ -4,29 +4,31 @@
 //
 //  Created by p0nyqz on 4.11.23..
 //
-
 import Foundation
 
 var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
+
+
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
-        fatalError("Coudn't find \(filename) in main bundle.")
+        fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
+
+
     do {
         data = try Data(contentsOf: file)
     } catch {
-        fatalError("Coudn't load \(filename) from main bundle:\n\(error)")
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
+
+
     do {
         let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from:data)
+        return try decoder.decode(T.self, from: data)
     } catch {
-        fatalError("Coudn't parse \(filename) as \(T.self):\n\(error)")
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
