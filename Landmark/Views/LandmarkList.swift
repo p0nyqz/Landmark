9167022ed590e970337d063df5321481a4b6583c
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-11-13 23:50:46 +0100

    feat: static list Landmark created

---
+++ Landmark/Views/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmark
+//
+//  Created by p0nyqz on 13.11.23..
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
